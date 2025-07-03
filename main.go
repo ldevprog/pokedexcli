905c830913efbc9dc9e64c3ca1b2186570ef58ce
levon-dalakyan <wman8800@gmail.com>
2025-07-03 18:08:13 +0300

    add a simple repl

--- main.go
+++ main.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex >")
+		scanner.Scan()
+		input := scanner.Text()
+		inputCleaned := cleanInput(input)
+		firstWord := inputCleaned[0]
+		fmt.Printf("Your command was: %s\n", firstWord)
+	}
 }
 
 func cleanInput(text string) []string {
